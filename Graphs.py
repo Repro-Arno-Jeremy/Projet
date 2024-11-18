34f77d89ed98ec195b83345a64a82fec976165a8
Jérémy Brunet <jeremy.brunet@insa-rennes.fr>
2024-11-18 09:10:55 +0100

    Fin graphe

--- Graphs.py
+++ Graphs.py
@@ -1,73 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#data = pd.read_csv("la_liga_data.csv")
-#data['year'] = data['year'].astype(int).apply(lambda x: f"{str(x)[-2:]}-{str(x + 1)[-2:]}")
-#data = data.set_index(["year"])  # On remet les index multi-niveaux
-
-#annees_uniques = data.index.get_level_values('year').unique().to_list()
-
-""" def create_graph1(): 
-    parameters = ["xG"]
-    fig, ax = plt.subplots()
-    plt.title("La_liga")
-    for parameter in parameters:             
-        ax.plot(annees_uniques, mean_value(parameter), color="orange")
-        ax.scatter(annees_uniques, mean_value(parameter), color="orange", marker="o", label="Points individuels")
-    plt.ylabel("Mean gained points per match")
-    plt.show()
-
-def mean_value(parameter):
-    values = []
-    for annee in annees_uniques:
-        values.append(data.loc[annee].mean()[parameter])
-    return values """
-
 # Charger les données
-data = pd.read_csv('la_liga_data.csv')
 
-# Extraire les valeurs xG de domicile et extérieur en les convertissant en type numérique
-#data['xG_home'] = data['xG'].apply(lambda x: eval(x)['h']).astype(float)
-#data['xG_away'] = data['xG'].apply(lambda x: eval(x)['a']).astype(float)
+def create_graph(league):
+    data = pd.read_csv(league + '.csv')
 
-# Extraire les probabilités de forecast
-data['forecast_home'] = data['forecast'].apply(lambda x: eval(x))
-data['forecast_away'] = data['forecast'].apply(lambda x: eval(x))
-data['goals'] = data['goals'].apply(lambda x: eval(x))
+    # Extraire les probabilités de forecast
+    data['forecast_home'] = data['forecast'].apply(lambda x: eval(x))
+    data['forecast_away'] = data['forecast'].apply(lambda x: eval(x))
+    data['goals'] = data['goals'].apply(lambda x: eval(x))
 
-# Calculer les points attendus (xP) pour domicile et extérieur
-data['xP_home'] = data['forecast_home'].apply(lambda f: 3 * float(f['w']) + 1 * float(f['d']))
-data['xP_away'] = data['forecast_away'].apply(lambda f: 3 * float(f['l']) + 1 * float(f['d']))
-data['results'] = data['goals'].apply(lambda f: 3 if float(f['h']) > float(f['a']) else 1 if float(f['h']) == float(f['a']) else 0)
-data['results_away'] = data['goals'].apply(lambda f: 3 if float(f['a']) > float(f['h']) else 1 if float(f['h']) == float(f['a']) else 0)
+    # Calculer les points attendus (xP) pour domicile et extérieur
+    data['xP_home'] = data['forecast_home'].apply(lambda f: 3 * float(f['w']) + 1 * float(f['d']))
+    data['xP_away'] = data['forecast_away'].apply(lambda f: 3 * float(f['l']) + 1 * float(f['d']))
+    data['results'] = data['goals'].apply(lambda f: 3 if float(f['h']) > float(f['a']) else 1 if float(f['h']) == float(f['a']) else 0)
+    data['results_away'] = data['goals'].apply(lambda f: 3 if float(f['a']) > float(f['h']) else 1 if float(f['h']) == float(f['a']) else 0)
 
-# Calculer la moyenne de xG pour les équipes à domicile et à l'extérieur pour chaque saison
-#average_xG_home = data.groupby('year')['xG_home'].mean()
-#average_xG_away = data.groupby('year')['xG_away'].mean()
 
-# Calculer la moyenne de xP pour les équipes à domicile et à l'extérieur par saison
-average_xP_home = data.groupby('year')['xP_home'].mean()
-average_xP_away = data.groupby('year')['xP_away'].mean()
-average_results = data.groupby('year')['results'].mean()
-average_results_away = data.groupby('year')['results_away'].mean()
+    # Calculer la moyenne de xP pour les équipes à domicile et à l'extérieur par saison
+    average_xP_home = data.groupby('year')['xP_home'].mean()
+    average_xP_away = data.groupby('year')['xP_away'].mean()
+    average_results = data.groupby('year')['results'].mean()
+    average_results_away = data.groupby('year')['results_away'].mean()
 
-# Affichage des résultats
-#for year in average_xG_home.index:
-#    print(f"Saison {year} - xG moyen domicile : {average_xG_home[year]:.2f}, xG moyen extérieur : {average_xG_away[year]:.2f}")
-
-def create_graph():
+    #Création graphe
     fig, ax = plt.subplots()
-    plt.title("La_liga")
+    plt.title(league)
     ax.plot(average_xP_home.index, average_xP_home, color="orange", label="xP_home")
-    ax.scatter(average_xP_home.index, average_xP_home, color="orange", marker="o", label="xP_home")
+    ax.scatter(average_xP_home.index, average_xP_home, color="orange", marker="o")
     ax.plot(average_xP_away.index, average_xP_away, color="red", label="xP_away")
-    ax.scatter(average_xP_away.index, average_xP_away, color="red", marker="o", label="xP_away")
+    ax.scatter(average_xP_away.index, average_xP_away, color="red", marker="o")
     ax.plot(average_results.index, average_results, color="blue", label="results")
-    ax.scatter(average_results.index, average_results, color="blue", marker="o", label="result")
+    ax.scatter(average_results.index, average_results, color="blue", marker="o")
     ax.plot(average_results_away.index, average_results_away, color="green", label="results_away")
-    ax.scatter(average_results_away.index, average_results_away, color="green", marker="o", label="result_away")
+    ax.scatter(average_results_away.index, average_results_away, color="green", marker="o")
 
     plt.ylabel("Mean gained points per match")
+    plt.legend(fontsize=10)
     plt.show()
 
-create_graph()
+create_graph('la_liga_data')
