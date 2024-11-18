aea587881446c47a6a2c8b9921a17422b071b658
arnolec <arno.lecrivain@insa-rennes.fr>
2024-11-18 09:02:03 +0100

    Ajout results et results_away

--- Graphs.py
+++ Graphs.py
@@ -33,10 +33,13 @@ data = pd.read_csv('la_liga_data.csv')
 # Extraire les probabilités de forecast
 data['forecast_home'] = data['forecast'].apply(lambda x: eval(x))
 data['forecast_away'] = data['forecast'].apply(lambda x: eval(x))
+data['goals'] = data['goals'].apply(lambda x: eval(x))
 
 # Calculer les points attendus (xP) pour domicile et extérieur
 data['xP_home'] = data['forecast_home'].apply(lambda f: 3 * float(f['w']) + 1 * float(f['d']))
 data['xP_away'] = data['forecast_away'].apply(lambda f: 3 * float(f['l']) + 1 * float(f['d']))
+data['results'] = data['goals'].apply(lambda f: 3 if float(f['h']) > float(f['a']) else 1 if float(f['h']) == float(f['a']) else 0)
+data['results_away'] = data['goals'].apply(lambda f: 3 if float(f['a']) > float(f['h']) else 1 if float(f['h']) == float(f['a']) else 0)
 
 # Calculer la moyenne de xG pour les équipes à domicile et à l'extérieur pour chaque saison
 #average_xG_home = data.groupby('year')['xG_home'].mean()
@@ -45,6 +48,8 @@ data['xP_away'] = data['forecast_away'].apply(lambda f: 3 * float(f['l']) + 1 * 
 # Calculer la moyenne de xP pour les équipes à domicile et à l'extérieur par saison
 average_xP_home = data.groupby('year')['xP_home'].mean()
 average_xP_away = data.groupby('year')['xP_away'].mean()
+average_results = data.groupby('year')['results'].mean()
+average_results_away = data.groupby('year')['results_away'].mean()
 
 # Affichage des résultats
 #for year in average_xG_home.index:
@@ -57,6 +62,10 @@ def create_graph():
     ax.scatter(average_xP_home.index, average_xP_home, color="orange", marker="o", label="xP_home")
     ax.plot(average_xP_away.index, average_xP_away, color="red", label="xP_away")
     ax.scatter(average_xP_away.index, average_xP_away, color="red", marker="o", label="xP_away")
+    ax.plot(average_results.index, average_results, color="blue", label="results")
+    ax.scatter(average_results.index, average_results, color="blue", marker="o", label="result")
+    ax.plot(average_results_away.index, average_results_away, color="green", label="results_away")
+    ax.scatter(average_results_away.index, average_results_away, color="green", marker="o", label="result_away")
 
     plt.ylabel("Mean gained points per match")
     plt.show()
